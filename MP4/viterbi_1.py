cff0ca6fa72af3f73cc247df58f5e0427cf66de5
Yan-Jun Fang <yjfang2@illinois.edu>
2021-10-21 22:47:55 -0500

    basic for transition done

--- MP4/viterbi_1.py
+++ MP4/viterbi_1.py
@@ -18,30 +18,36 @@ def viterbi_1(train, test):
     emission, start, transition = {}, {}, {} #dict of dict like baseline
     #emission: key = type, value = dict, key = word, value = freq
     #start: key = type, val = freq, only tracks when at start
+    #transition: key = (type_k, type_k-1), val = freq
     emission_n = {} #key: type, value: # of words of the type
     #emission: k = tag, v = dict --> dict: k = word, v = freq
     #so P(wi | ti) = emission[tag][word] / 
     for s in train: 
-        start = True
-        for pair in s:
-            word, tag = pair[0], pair[1]
-            if start == True:
+        for i in range(len(s)):
+            word, tag = s[i][0], s[i][1]
+            if i == 0: #start
                 if start.get(tag, None) == None:
                     start[tag] = 1
                 else:
                     start[tag] += 1
                 start = False
 
-            if emission_n.get(tag, None) == None:
+            if emission_n.get(tag, None) == None: #emission
                 emission_n[tag] = 1
             else:
                 emission_n[tag] += 1
-            
             type_dict = emission.get(tag, None)
             if type_dict == None:
                 type_dict[tag] = {word: 1}
             else:
                 type_dict[tag] += 1
+
+            if i == 0: continue #skip transition for first one
+            prev_tag = s[i - 1][1] 
+            if transition.get((tag, prev_tag), None) == None:
+                transition[(tag, prev_tag)] = 1
+            else:
+                transition[(tag, prev_tag)] += 1
 
 
 
