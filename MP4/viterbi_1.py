347770b63d1d6694dd163d941a2a284e3a730215
Yan-Jun Fang <yjfang2@illinois.edu>
2021-10-21 23:45:05 -0500

    working on backtracking

--- MP4/viterbi_1.py
+++ MP4/viterbi_1.py
@@ -4,6 +4,7 @@ but it should do better than the baseline at words with multiple tags (because n
 to predict the tag).
 """
 
+import math
 def viterbi_1(train, test):
     '''
     input:  training data (list of sentences, with tags on the words)
@@ -16,10 +17,10 @@ def viterbi_1(train, test):
 
     #Pe: P(wi | ti) = P(witi) / P(ti)
     emission, start, transition = {}, {}, {} #dict of dict like baseline
-    #emission: key = type, value = dict, key = word, value = freq
-    #start: key = type, val = freq, only tracks when at start
-    #transition: key = (type_k, type_k-1), val = freq
-    emission_n = {} #key: type, value: # of words of the type
+    #emission: key = tag, value = dict, key = word, value = freq
+    #start: key = tag, val = freq, only tracks when at start
+    #transition: key = (tag_k, tag_k-1), val = freq
+    emission_n = {} #key: tag, value: # of words of the tag
     #emission: k = tag, v = dict --> dict: k = word, v = freq
     #so P(wi | ti) = emission[tag][word] / 
     for s in train: 
@@ -36,11 +37,11 @@ def viterbi_1(train, test):
                 emission_n[tag] = 1
             else:
                 emission_n[tag] += 1
-            type_dict = emission.get(tag, None)
-            if type_dict == None:
-                type_dict[tag] = {word: 1}
+            tag_dict = emission.get(tag, None)
+            if tag_dict == None:
+                tag_dict[tag] = {word: 1}
             else:
-                type_dict[tag] += 1
+                tag_dict[tag] += 1
 
             if i == 0: continue #skip transition for first one
             prev_tag = s[i - 1][1] 
@@ -49,9 +50,46 @@ def viterbi_1(train, test):
             else:
                 transition[(tag, prev_tag)] += 1
 
+    #decoding
+    ans = []
+    for s in test:
+        v, b = [], []
+        for k in range(len(s)):
+            tags = list(start.keys())
+            v_curr, b_curr = [], []
+            word = s[k][0]
 
+            max_v = 0, best_t = tags[0]
+            for tB in range(len(tags)):  
+                tag = tags[tB]
+                if k == 0:
+                    v_curr.append(math.log(start[tag]) + math.log(emission[tag][word]))
+                    b_curr.append(tag)
+                else:
+                    for tA in range(len(tags)):
+                        curr_v  = math.log(v[k - 1][tA]) 
+                        curr_v += math.log(transition[(tag, tags[tA])])
+                        curr_v += math.log(emission[tag][word])
 
+                        if curr_v > max_v:
+                            max_v = curr_v
+                            best_t = tags[tB]
 
+                    v_curr.append(max_v)
+                    b_curr.append(best_t)
 
+            v.append(v_curr)
+            b.append(b_curr)
+        
+        #find best one in last row
+        best_idx = 0, best_odds = v[len(s) - 1][0]
+        for t in range(1, len(tags)):
+            if v[len(s) - 1][t] > best_odds:
+                best_odds = v[len(s) - 1][t]
+                best_idx  = t
 
-    return []
+        best_end_tag = tags[best_idx]
+        #backtrack 
+        
+        
+    return ans
