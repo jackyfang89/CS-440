51f4a74a46d6bd764cba7f15c1eb175b223f6c01
Yan-Jun Fang <yjfang2@illinois.edu>
2021-10-22 14:04:54 -0500

    some bugs fixed, noun not getting tagged correctly

--- MP4/viterbi_1.py
+++ MP4/viterbi_1.py
@@ -19,21 +19,20 @@ def viterbi_1(train, test):
     emission, transition = {}, {} #dict of dict like baseline
     #emission: key = tag, value = dict, key = word, value = freq
     #transition: key = prev tag, value = dict, key = tag, value = freq
-    emission_n = {} #key: tag, value: # of words of the tag
+    emission_n, transition_n = {}, {} #key: tag, value: # of words of the tag
     #emission: k = tag, v = dict --> dict: k = word, v = freq
     #so P(wi | ti) = emission[tag][word] / 
     for s in train: 
         for i in range(len(s)):
             word, tag = s[i][0], s[i][1]
-            if emission_n.get(tag, None) == None: #emission
+            if emission_n.get(tag, None) == None: #total counts in emission for each tag
                 emission_n[tag] = 1
             else:
                 emission_n[tag] += 1
 
             tag_dict = emission.get(tag, None)
             if tag_dict == None:
-                temp = {word: 1}
-                emission[tag] = temp
+                emission[tag] = {word: 1}
             else:
                 if tag_dict.get(word, None) == None:
                     emission[tag][word] = 1
@@ -42,10 +41,20 @@ def viterbi_1(train, test):
 
             if i == 0: continue #skip transition for first one
             prev_tag = s[i - 1][1] 
-            if transition.get(prev_tag, None) == None:
+
+            if transition_n.get(prev_tag, None) == None: #total transitions for each prev tag
+                transition_n[prev_tag] = 1
+            else:
+                transition_n[prev_tag] += 1
+
+            trans_dict = transition.get(prev_tag, None)
+            if trans_dict == None:
                 transition[prev_tag] = {tag: 1}
             else:
-                transition[prev_tag][tag] += 1
+                if trans_dict.get(tag, None) == None:
+                    transition[prev_tag][tag] = 1
+                else:
+                    transition[prev_tag][tag] += 1
 
     #decoding
     ans = []
@@ -55,6 +64,12 @@ def viterbi_1(train, test):
 
     tags = list(emission_n.keys())
 
+    # for tag in tags:
+    #     if emission[tag].get('plainfield', None) == None:
+    #         print("None")
+    #     else:
+    #         print(emission[tag]['plainfield'])
+
     for s in test:
         v, b = [], []
         for k in range(len(s)):
@@ -62,44 +77,47 @@ def viterbi_1(train, test):
             v_curr, b_curr = [], []
             word = s[k]
 
-            for tB in range(len(tags)):  
-                tag = tags[tB]
+            for tagB in tags:  
+                # tag = tags[tB]
                 #values for emission smoothing
-                e_V = len(emission[tag].keys())
-                e_denom = emission_n[tag] + e_alpha * (e_V + 1)
+                e_V = len(emission[tagB].keys())
+                e_denom = emission_n[tagB] + e_alpha * (e_V + 1)
 
                 if k == 0:
                     #start
                     curr_v = 0
-                    if emission[tag].get('START', None) == None:
+                    if emission[tagB].get('START', None) == None:
                         curr_v = math.log(e_alpha / e_denom)
                     else:
-                        curr_v = math.log((e_alpha + emission[tag]['START']) / e_denom)
+                        curr_v = math.log((e_alpha + emission[tagB]['START']) / e_denom)
                         
-                    if emission[tag].get(word, None) == None: #smoothing for emission
+                    if emission[tagB].get(word, None) == None: #smoothing for emission
                         curr_v += math.log(e_alpha / e_denom)
                     else:
-                        curr_v += math.log((e_alpha + emission[tag][word]) / e_denom)
+                        curr_v += math.log((e_alpha + emission[tagB][word]) / e_denom)
 
                     v_curr.append(curr_v)
                     b_curr.append(-1)
                 else:
-                    t_V = len(transition.keys())
-                    t_denom = len(s) - 1 + t_alpha * (t_V + 1) #total num of transitions is len(s) - 1
-                    max_v, best_t = 0, 0
+                    max_v, best_t = float('-inf'), 0
 
                     for tA in range(len(tags)):
+                        tagA = tags[tA]
                         curr_v = v[k - 1][tA]
-
-                        if transition.get((tag, tags[tA]), None) == None:
-                            curr_v += math.log(t_alpha / t_denom)
+                        if transition.get(tagA, None) == None: #tagA = END usually
+                            curr_v += math.log(0.0001) #set to some tiny value
                         else:
-                            curr_v += math.log(t_alpha + transition[(tag, tags[tA])] / t_denom)
+                            t_V = len(transition[tagA].keys())
+                            t_denom = transition_n[tagA] + t_alpha * (t_V + 1) 
+                            if transition[tagA].get(tagB, None) == None:
+                                curr_v += math.log(t_alpha / t_denom)
+                            else:
+                                curr_v += math.log((t_alpha + transition[tagA][tagB]) / t_denom)
 
-                        if emission[tag].get(word, None) == None:
+                        if emission[tagB].get(word, None) == None:
                             curr_v += math.log(e_alpha / e_denom)
                         else:
-                            curr_v += math.log((e_alpha + emission[tag][word]) / e_denom)
+                            curr_v += math.log((e_alpha + emission[tagB][word]) / e_denom)
 
                         if curr_v > max_v:
                             max_v = curr_v
@@ -112,6 +130,7 @@ def viterbi_1(train, test):
             b.append(b_curr)
         
         if s == test[0]:
+            print(emission_n.keys())
             for i in range(len(b)):
                 for j in range(len(b[i])):
                     print(b[i][j], end = ", ")
@@ -127,11 +146,15 @@ def viterbi_1(train, test):
             if v[len(s) - 1][t] > best_odds:
                 best_odds = v[len(s) - 1][t]
                 best_idx  = t
-        
+        # if s == test[0]:
+        #     print("best_idx: " + str(best_idx))
         #backtrack 
         curr_idx = best_idx
         curr_s_tags = [tags[curr_idx]]
-        for i in range(len(s) - 1, -1, -1):
+        # if s == test[0]:
+        #     print("tags best_idx: " + str(tags[curr_idx]))
+
+        for i in range(len(s) - 1, 0, -1):
             curr_idx = b[i][curr_idx]
             curr_s_tags.append(tags[curr_idx])
 
@@ -141,7 +164,7 @@ def viterbi_1(train, test):
         for i in range(len(s)):
             curr_ans.append((s[i], curr_s_tags[i]))
         
-        if s == test[0] or s == test[1]:
+        if s == test[0]:
             print("curr_ans: " + str(curr_ans))
             print("s: " + str(s))
 
